800585d6b4da9c517c37fa6e117fac6cf4b701fa
Kyrylo Pyniv <pynivkm@gmail.com>
2021-08-08 22:33:41 +0300

    Sol

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
+const tRows = document.querySelectorAll('tr');
+
+for (let i = 0; i < tRows.length; i++) {
+  const clonedCell = tRows[i].children[1].cloneNode(true);
+
+  /* clonedCell.cloneNode(); */
+
+  tRows[i].insertBefore(clonedCell,
+    tRows[i].children[tRows[i].children.length - 1]);
+};
+
 // write your code here
